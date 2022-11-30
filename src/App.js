3198d3dd5132e6ee6314bdf244be5db652146c99
Badhusha <37366257+Badhusha-Akhthaar@users.noreply.github.com>
2022-11-30 16:43:45 +0530

    removed _redirect file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import { ShellBar } from "@ui5/webcomponents-react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main";
